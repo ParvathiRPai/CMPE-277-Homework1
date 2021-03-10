d9b2c0e3e47c1bdef8e881fb733b9360b24092b7
Parvathi <pava@uw.edu>
2021-03-10 11:12:57 -0800

    second page

--- app/src/main/java/com/codepath/quizapp/Constants.kt
+++ app/src/main/java/com/codepath/quizapp/Constants.kt
@@ -3,7 +3,8 @@ package com.codepath.quizapp
 object Constants{
     fun getQuestions(): ArrayList<Question>{
         val questionsList=ArrayList<Question>()
-        val question1=Question(1,"What country does this flag belong to?",R.drawable.ic_flag_of_argentina,"argentina","Australia","Armenia","Austria",1)
+        val question1=Question(1,"What country does this flag belong to?",R.drawable.ic_flag_of_argentina,
+            "Argentina","Australia","Armenia","Austria",1)
         questionsList.add(question1)
 
         val question2 = Question(
